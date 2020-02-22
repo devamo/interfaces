5697f44b927276326580d7627c73a1539fcf0565
Yaroslav Shelomentsev <ya@devio.pro>
2020-02-22 13:04:33 +0300

    v1

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { AccountInterface } from './interfaces/Account'
+import { ProductInterface } from './interfaces/Product'
+import { AccountProductInterface } from './interfaces/AccountProduct'
+import { AccountUserInterface } from './interfaces/AccountUser'
+import { AccountUserGroupInterface } from './interfaces/AccountUserGroup'
+
+export {
+  AccountInterface,
+  ProductInterface,
+  AccountProductInterface,
+  AccountUserInterface,
+  AccountUserGroupInterface
+}
